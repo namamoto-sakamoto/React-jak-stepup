3248eb20ee51d3820a4ced0738d861e3e1fb25dd
yuko-sakamoto <emaraku1009@gmail.com>
2022-09-20 03:06:34 +0000

    Update section3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState, useCallback, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+  
+  const onChangeText = (event) => setText(event.target.value);
+  
+  const onClickOpen = () => setOpen(!open);
+  
+  const onClickClose = () => useCallback(setOpen(false), [setOpen]);
+  
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+  
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+        <input value={text} onChenge={onChangeText} />
+        <br />
+        <br />
+        <button onClick={onClickOpen}>表示</button>
+        <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
